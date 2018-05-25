f396f0bd01e6d3123883388b96cb36e8403b7b08
Zohar <zkapach@hotmail.com>
2018-05-25 12:42:59 -0400

    updates to hog svm lib

--- lib/ntd/hog_svm.py
+++ lib/ntd/hog_svm.py
@@ -113,55 +113,131 @@ def img_features(feature_image, feat_type, hist_bins, orient,
         file_features.append(hog_features)
     return file_features
 
-def extract_features(pyroidb, feat_type, spatial_size=(32, 32),
+def extract_pyroidb_features(pyroidb, feat_type, spatial_size=(32, 32),
                         hist_bins=32, orient=9, 
                         pix_per_cell=8, cell_per_block=2, hog_channel=0):
     # Create a list to append feature vectors to
     features = []
     y = []
+    coco_feat = []
+    voc_feat = []
+    imageNet_feat = []
+    cam2_feat = []
+    inria_feat = []
+    caltech_feat = []
+    sun_feat = []
+    kitti_feat = []
+
+    coco_idx = []
+    voc_idx = []
+    imageNet_idx = []
+    cam2_idx = []
+    inria_idx = []
+    caltech_idx = []
+    sun_idx = []
+    kitti_idx = []
+
+    errors = 0
+
     # Iterate through the list of images
-    for file_p in pyroidb:
-        file_features = []
-        image = file_p[0] # Read in each imageone by one
-      #  print(type(image))
-            # apply color conversion if other than 'RGB'
-        if isinstance(image, np.ndarray) == True:
-            feature_image = np.copy(image)      
-            file_features = img_features(feature_image, feat_type, hist_bins, orient, pix_per_cell, cell_per_block, hog_channel)    
-            features.append(np.concatenate(file_features))
-            y.append(file_p[1])
+    for i in range(pyroidb.__len__()):
 
-        # if mixed we need to do something like this
-        # if file_p[1] == 1:
-        #     y.append(1)
-        # else if file_p[1] == 2:
-        #     y.append(2)
-        # else if file_p[1] == 3:    
-        #     y.append(3)
-        # else if file_p[1] == 4:   
-        #     y.append(4)
-        # else if file_p[1] == 5:    
-        #     y.append(5)
-        # else if file_p[1] == 6:    
-        #     y.append(6)
-        # else if file_p[1] == 7:    
-        #     y.append(7)
-        # else if file_p[1] == 8:    
-        #     y.append(8)
+    # for file_p in pyroidb:
+        try:
+            file_features = []
+            image = pyroidb[i][0] # Read in each imageone by one
+          #  print(type(image))
+                # apply color conversion if other than 'RGB'
+            if isinstance(image, np.ndarray) == True: 
+                feature_image = np.copy(image)      
+                file_features = img_features(feature_image, feat_type, hist_bins, orient, pix_per_cell, cell_per_block, hog_channel)    
+                # features.append(np.concatenate(file_features))
+                y.append(pyroidb[i][1])
 
-        
+                # if mixed we need to do something like this
+            if pyroidb[i][1] == 0:
+                coco_feat.append(np.concatenate(file_features))
+                coco_idx.append(i)
+            elif pyroidb[i][1] == 1:
+                voc_feat.append(np.concatenate(file_features))
+                voc_idx.append(i)
+            elif pyroidb[i][1] == 2:    
+                imageNet_feat.append(np.concatenate(file_features))
+                imageNet_idx.append(i)
+            elif pyroidb[i][1] == 3:   
+                cam2_feat.append(np.concatenate(file_features))
+                cam2_idx.append(i)
+            elif pyroidb[i][1] == 4:    
+                inria_feat.append(np.concatenate(file_features))
+                inria_idx.append(i)
+            elif pyroidb[i][1] == 5:    
+                caltech_feat.append(np.concatenate(file_features))
+                caltech_idx.append(i)
+            elif pyroidb[i][1] == 6:    
+                sun_feat.append(np.concatenate(file_features))
+                sun_idx.append(i)
+            elif pyroidb[i][1] == 7:    
+                kitti_feat.append(np.concatenate(file_features))
+                kitti_idx.append(i)
+        except: 
+            errors = errors + 1
+
+        # X = np.vstack((coco_feat, voc_feat, imageNet_feat, cam2_feat, inria_feat, caltech_feat, sun_feat, kitti_feat)).astype(np.float64)
+        X_idx = np.vstack((coco_idx, voc_idx, imageNet_idx, cam2_idx, inria_idx, caltech_idx, sun_idx, kitti_idx)).astype(np.float64)
+
+        y.sort()
+        print('number of errors = ', errors)
 #        feature_image=cv2.flip(feature_image,1) # Augment the dataset with flipped images
 #        file_features = img_features(feature_image, hist_bins, orient, 
 #                        pix_per_cell, cell_per_block, hog_channel)
 #        #changed from cancatenate
 #        features.append(file_features)
-    return features # Return list of feature vectors
+    return coco_feat, voc_feat, imageNet_feat, cam2_feat, inria_feat, caltech_feat, sun_feat, kitti_feat, y, X_idx # Return list of feature vectors
 
 
 
+def split_data(train_size, test_size, coco_feat, voc_feat, imagenet_feat, cam2_feat, inria_feat, caltech_feat, sun_feat, kitti_feat):
+    coco_train = coco_feat[0:train_size]
+    voc_train = voc_feat[0:train_size]
+    imagenet_train = imagenet_feat[0:train_size]
+    cam2_train = cam2_feat[0:train_size]
+    inria_train = inria_feat[0:train_size]
+    caltech_train = caltech_feat[0:train_size]
+    sun_train = sun_feat[0:train_size]
+    kitti_train = kitti_feat[0:train_size]
 
 
-def split_data()
+    try:
+        coco_test = coco_feat[train_size: train_size + test_size]
+    except: 
+        coco_test = coco_feat[train_size: train_size + len(coco_feat)]
+
+    try: 
+        voc_test = voc_feat[train_size: train_size + test_size]
+    except:
+        voc_test = voc_feat[train_size: train_size + len(voc_feat)]
+
+    try:
+        imagenet_test = imagenet_feat[train_size: train_size + test_size]
+    except: 
+        imagenet_test = imagenet_feat[train_size: train_size + len(imagenet_feat)]
+
+    try: 
+        cam2_test = cam2_feat[train_size: train_size + test_size]
+    except: 
+        cam2_test = cam2_feat[train_size: train_size + len(cam2_feat)]
+    try: 
+        inria_test = inria_feat[train_size: train_size + test_size]
+    try:
+        caltech_test = caltech_feat[train_size: train_size + test_size]
+    try: 
+        sun_test = sun_feat[train_size: train_size + test_size]
+    try:
+        kitti_test = kitti_feat[train_size: train_size + test_size]
+
+
+    return
+
 
 
 def scale_data(X_train, X_test):
@@ -171,7 +247,7 @@ def scale_data(X_train, X_test):
     X_train_scaled = X_train_scaler.transform(X_train)
     X_test_scaled = X_test_scaler.transform(X_test)
 
-return X_train_scaled, X_test_scaled
+    return X_train_scaled, X_test_scaled
 
 
 def train_SVM(X_train, y_train):
@@ -182,67 +258,23 @@ def train_SVM(X_train, y_train):
     model_fit = svc.fit(X_train, y_train)
     t2 = time.time()
     print(round(t2-t, 2), 'Seconds to train SVC...')
-return model_fit
+    return model_fit
 
 
 def make_confusion_matrix(model, X_test, y_test):
-        y_pred = model.predict(X_test)    
-        # Compute confusion matrix
-        cnf_matrix = confusion_matrix(y_test, y_pred)
-        np.set_printoptions(precision=2)
-        
-        # Plot normalized confusion matrix  ----- NEED TO FIX CLASS NAMES DEPENDS ON PYROIDB
-        class_names = ('COCO', 'Caltech', 'ImageNet', 'Pascal', 'Cam2', 'INRIA', 'Sun', 'Kitti', 'Mixed')
-        plt.figure()
-        plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True, title='Normalized confusion matrix')
-        plt.show()
-return
+    y_pred = model.predict(X_test)    
+    # Compute confusion matrix
+    cnf_matrix = confusion_matrix(y_test, y_pred)
+    np.set_printoptions(precision=2)
+    
+    # Plot normalized confusion matrix  ----- NEED TO FIX CLASS NAMES DEPENDS ON PYROIDB
+    class_names = ('COCO', 'Caltech', 'ImageNet', 'Pascal', 'Cam2', 'INRIA', 'Sun', 'Kitti', 'Mixed')
+    plt.figure()
+    plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True, title='Normalized confusion matrix')
+    plt.show()
+    return
 
 def test_acc(model, X_test, y_test):
-     print('Test Accuracy of SVC = ', round(model.score(X_test, y_test), 4)) # Check the score of the SVC
-return
-    
-    coco_train = coco_feat[0:dataset_size_train]
-    coco_test = coco_feat[dataset_size_train:(dataset_size_train+dataset_size_test)]
-    caltech_train = caltech_feat[0:dataset_size_train]
-    caltech_test = caltech_feat[dataset_size_train:(dataset_size_train+dataset_size_test)]
-    imagenet_train = imagenet_feat[0:dataset_size_train]
-    imagenet_test = imagenet_feat[dataset_size_train:(dataset_size_train+dataset_size_test)]
-    pascal_train = pascal_feat[0:dataset_size_train]
-    pascal_test = pascal_feat[dataset_size_train:(dataset_size_train+dataset_size_test)]
-    cam2_train = cam2_feat[0:dataset_size_train]
-    cam2_test = cam2_feat[dataset_size_train:(dataset_size_train+dataset_size_test)]
-    inria_train = inria_feat[0:dataset_size_train]
-    inria_test = inria_feat[dataset_size_train:(dataset_size_train+dataset_size_test)]
-    sun_train = sun_feat[0:dataset_size_train]
-    sun_test = sun_feat[dataset_size_train:(dataset_size_train+dataset_size_test)]
-    kitti_train = kitti_feat[0:dataset_size_train]
-    kitti_test = kitti_feat[dataset_size_train:(dataset_size_train+dataset_size_test)]
-    
-    
-    print('len after feat extract and cut')
-    print('len of coco', len(coco_train), len(coco_test))
-    print('len of caltech', len(caltech_train), len(caltech_test))
-    print('len of imagenet', len(imagenet_train), len(imagenet_test))
-    print('len of pascal' ,len(pascal_train), len(pascal_test))
-    print('len of cam2', len(cam2_train), len(cam2_test))
-    print('len of inria', len(inria_train), len(inria_test))
-    print('len of sun', len(sun_train), len(sun_test))
-    print('len of kitti', len(kitti_train), len(kitti_test))
-    print('len of mix', len(mix_train), len(mix_test))
-    
-    X_train_m = np.vstack((coco_train, caltech_train, imagenet_train, pascal_train, cam2_train, inria_train, sun_train, kitti_train, mix_train)).astype(np.float64)
-    X_test_m = np.vstack((coco_test, caltech_test, imagenet_test, pascal_test, cam2_test, inria_test, sun_test, kitti_test, mix_test)).astype(np.float64)
-
-    y_train = np.hstack((np.ones(len(coco_train)), np.full(len(caltech_train), 2), np.full(len(imagenet_train), 3), np.full(len(pascal_train), 4), np.full(len(cam2_train), 5), np.full(len(inria_train), 6), np.full(len(sun_train), 7), np.full(len(kitti_train), 8), np.full(len(mix_train), 9)))
-    y_test = np.hstack((np.ones(len(coco_test)), np.full(len(caltech_test), 2), np.full(len(imagenet_test), 3), np.full(len(pascal_test), 4), np.full(len(cam2_test), 5), np.full(len(inria_test), 6), np.full(len(sun_test), 7), np.full(len(kitti_test), 8), np.full(len(mix_test), 9)))
-    
-    
-
-    print('Test Accuracy of SVC = ', round(model_fit.score(X_test_scaled, y_test), 4)) # Check the score of the SVC
-    results.append(round(model_fit.score(X_test_scaled, y_test), 4))
-   
-    t4 = time.time()
-    print(round(t4-t3, 2), 'Seconds to run')
-
+    print('Test Accuracy of SVC = ', round(model.score(X_test, y_test), 4)) # Check the score of the SVC
+    return
 
