523b1cbdea6b96db4ecead3ae6e98c5ea2d4de91
gauenk <kent.gauen@gmail.com>
2018-06-01 11:37:35 -0400

    update coco.yml for gauenk

--- lib/datasets/factory.py
+++ lib/datasets/factory.py
@@ -32,6 +32,7 @@ def get_repo_imdb(name):
         raise KeyError('Dataset name [{}] is not correct length'.format(name))
     for __set in __sets:
         if di[0] == __set:
+            print(di)
             return RepoImdb(di[0],di[1],di[2])
             # if __set == "coco":
             #     return coco(di[0],di[1],di[2])
